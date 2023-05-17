df719b4ff9f05acc3b8cdf0276e8ae8a1467d2c2
Thaísa M. Predebon <110207226+thaisampredebon@users.noreply.github.com>
2023-05-17 15:36:04 -0300

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -87,9 +87,9 @@ st.sidebar.markdown('## Selecione a data limite:')
 
 date_slider = st.sidebar.slider(
     'Arraste o marcador até a data desejada.',
-    value = pd.to_datetime(2022, 4, 6),
-    min_value = pd.to_datetime(2022, 2, 11),
-    max_value = pd.to_datetime(2022, 4, 6),
+    value = pd.datetimes(2022, 4, 6),
+    min_value = pd.datetimes(2022, 2, 11),
+    max_value = pd.datetimes(2022, 4, 6),
     format = 'DD-MM-YYYY',
     label_visibility = 'collapsed')
    
